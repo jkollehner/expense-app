5f2d17a1ce9d22612fe84985cc138e00e604232e
jkollehner <jkollehner@gmail.com>
2026-02-14 16:42:36 -0600

    Improve service worker caching and activation

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
-const CACHE_NAME = "expense-app-v1.5";
+const CACHE_NAME = "expense-app-v1.6";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "index.html"
+        "./",
+        "./index.html"
       ]);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
