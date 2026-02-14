dd8208df9d1eb323828fcc200509586192642ba6
jkollehner <jkollehner@gmail.com>
2026-02-14 16:36:11 -0600

    Fix service worker caching path

--- sw.js
+++ sw.js
@@ -1,2 +1,19 @@
-YourRepositoryName/
-  index.html
+const CACHE_NAME = "expense-app-v1.5";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
